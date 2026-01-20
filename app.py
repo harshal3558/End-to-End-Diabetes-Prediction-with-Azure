f24322aa42111a4d823a31bb9b2f7b39c38abd5b
Harshal <harshal3558@gmail.com>
2026-01-20 21:23:17 +0530

    files are updated

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
+
 from src.DPAZ.logger import logging
 from src.DPAZ.exception import CustomException
+from src.DPAZ.components.data_ingestion import DataIngestion
+from src.DPAZ.components.data_ingestion import DataIngestionConfig
+from src.DPAZ.components.data_transformation import DataTransformation
+from src.DPAZ.components.data_transformation import DataTransformationConfig
+from src.DPAZ.components.model_tranier import ModelTrainerConfig
+from src.DPAZ.components.model_tranier import ModelTrainer
+
 import sys
 
 if __name__ == "__main__":
-    logging.info("The execution has started.")
-
+    logging.info("The execution has started")
+    
     try:
-        a = 1/10
+        # data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion() 
+        # data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        # data_transformation_config=DataIngestionConfig()
+        data_transformation=DataTransformation()
+        # data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
